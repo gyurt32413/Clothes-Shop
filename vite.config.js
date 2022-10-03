5ad820126cb34959ca13c0a09a68236c9200de99
gyurt32413 <gyurt32413@gmail.com>
2022-10-04 00:52:12 +0800

    vite config setting

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./clothes-E-Store",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/clothes-E-Store" : "/",
 });
