7b32c7bc10db3c70fd17f6bc6aa998d1f20ab00d
gyurt32413 <gyurt32413@gmail.com>
2022-10-04 01:39:40 +0800

    add deploy sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/gyurt32413/clothes-E-Store main:gh-pages
+cd-
