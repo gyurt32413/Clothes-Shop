3f349c3fca3a4cf61fa4a3c9f4f68d6361aabc6d
gyurt32413 <gyurt32413@gmail.com>
2022-10-04 17:00:50 +0800

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/clothes-E-Store",
+  base: "/Clothes-Shop",
   plugins: [vue()],
   resolve: {
     alias: {
